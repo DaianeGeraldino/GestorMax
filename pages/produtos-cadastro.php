713bf1bf523a122a4a5d8ec77033e1b070297acf
Gabriel-Buffon <gabrielbuffon989@gmail.com>
2025-06-02 21:11:09 -0300

    Update produtos-cadastro.php

--- pages/produtos-cadastro.php
+++ pages/produtos-cadastro.php
@@ -6,7 +6,7 @@
   <title>GESTORMAX - Cadastrar Produto</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="css/produtos.css">
+  <link rel="stylesheet" href="styles/produtos.css">
   <link rel="stylesheet" href="./css/sidebar.css">
 </head>
 <body>
