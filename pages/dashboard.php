b396611f0bf205b604f9860166c99c52f5b73b21
Gabriel-Buffon <gabrielbuffon989@gmail.com>
2025-06-02 21:10:05 -0300

    Update dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -6,7 +6,7 @@
   <title>GESTORMAX - Dashboard</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="css/dashboard.css">
+  <link rel="stylesheet" href="styles/dashboard.css">
   <link rel="stylesheet" href="./css/sidebar.css">
 </head>
 <body>
