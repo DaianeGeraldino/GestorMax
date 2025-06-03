4c9bc31d9e3371d9e5dd23bf56d669403de29c73
Gabriel-Buffon <gabrielbuffon989@gmail.com>
2025-06-02 21:08:58 -0300

    Update criar-categoria.php

--- pages/criar-categoria.php
+++ pages/criar-categoria.php
@@ -7,7 +7,7 @@
     <title>GESTORMAX - Criar Categoria</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/usuarios.css">
+    <link rel="stylesheet" href="styles/usuarios.css">
     <link rel="stylesheet" href="css/sidebar.css">
 
     <script>
