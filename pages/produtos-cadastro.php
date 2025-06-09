637b383c7114486c8a67e2f1314a5cdf4dd8cf86
Daiane Geraldino <129515871+DaianeGeraldino@users.noreply.github.com>
2025-06-09 20:59:54 -0300

    Update produtos-cadastro.php

--- pages/produtos-cadastro.php
+++ pages/produtos-cadastro.php
@@ -13,64 +13,7 @@
   <div class="container-fluid">
     <div class="row">
       <!-- Sidebar -->
-      <div class="col-md-3 col-lg-2 d-md-block bg-dark sidebar">
-        <div class="position-sticky pt-3 vh-100 d-flex flex-column">
-          <div class="sidebar-header text-center mb-4 px-3">
-            <h1 class="h4 text-white">GESTORMAX</h1>
-          </div>
-          
-          <nav class="flex-grow-1">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link text-white" href="dashboard.php">
-                  <i class="bi bi-house-door me-2"></i>
-                  Início
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="produtos-lista.php">
-                  <i class="bi bi-box-seam me-2"></i>
-                  Produtos
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active text-white" href="produtos-cadastro.php">
-                  <i class="bi bi-plus-circle me-2"></i>
-                  Cadastrar produto
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="vendas.php">
-                  <i class="bi bi-cart me-2"></i>
-                  Vendas
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="usuarios-lista.php">
-                  <i class="bi bi-people me-2"></i>
-                  Usuários
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="estatisticas.php">
-                  <i class="bi bi-bar-chart-line me-2"></i>
-                  Estatísticas
-                </a>
-              </li>
-            </ul>
-          </nav>
-          
-          <div class="sidebar-footer mt-auto p-3">
-            <div class="d-flex justify-content-between align-items-center mb-3" >
-              <span class="text-white" id="user-name">Administrador</span>
-            </div>
-            <button id="logout-btn" class="btn btn-outline-light w-100" onclick="location.href='login.php'">
-              <i class="bi bi-box-arrow-right me-2"></i>
-              Sair
-            </button>
-          </div>
-        </div>
-      </div>
+       <?php include '../INCLUDE/sidebar.php'; ?>
 
       <!-- Main content -->
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-4">
