a63eb22b2397557d2891a5c654fa0326c2303b0e
Daiane Geraldino <129515871+DaianeGeraldino@users.noreply.github.com>
2025-06-17 16:28:34 -0300

    Update dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -7,7 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
   <link rel="stylesheet" href="styles/dashboard.css">
-  <link rel="stylesheet" href="./css/sidebar.css">
+  <link rel="stylesheet" href="../styles/sidebar.css">
 </head>
 <body>
   <div class="container-fluid">
