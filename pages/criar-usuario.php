856646af4716b0f4abed988347d430398231db86
Daiane Geraldino <129515871+DaianeGeraldino@users.noreply.github.com>
2025-06-17 16:28:16 -0300

    Update criar-usuario.php

--- pages/criar-usuario.php
+++ pages/criar-usuario.php
@@ -43,8 +43,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <title>GESTORMAX - Criar Usuário</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="styles/usuarios.css">
-  <link rel="stylesheet" href="css/sidebar.css">
+  <link rel="stylesheet" href="../styles/usuarios.css">
+<link rel="stylesheet" href="../styles/sidebar.css">
 </head>
 <body>
   <div class="container-fluid">
