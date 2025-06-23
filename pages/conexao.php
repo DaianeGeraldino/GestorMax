3b969e7da9bf7864e0f15c5da01cf3028e927330
viniciussousaifes <viniciusjogador@ymail.com>
2025-06-23 19:34:53 -0300

    ...1

--- pages/conexao.php
+++ pages/conexao.php
@@ -6,8 +6,9 @@ $host = 'localhost';
 $usuario = 'root';
 $senha = 'serra';
 $banco = 'gestorMax';
+$port = '3306';
 
-$conn = new mysqli($host, $usuario, $senha, $banco);
+$conn = new mysqli($host, $usuario, $senha, $banco,$port);
 
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
