2ed444793ac2353dee37a425acc25fe493205b01
Gabriel-Buffon <gabrielbuffon989@gmail.com>
2025-06-24 22:19:51 -0300

    Update estatisticas.php, script adicionado no final

--- pages/estatisticas.php
+++ pages/estatisticas.php
@@ -260,7 +260,9 @@
       </main>
     </div>
   </div>
-
+  
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <script src="../js/estatisticas.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
